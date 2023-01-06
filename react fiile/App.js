c9dd1e92aee2e0d63aa0a129fc7bae81dac49064
Komal Mandal <56351799+Komalone@users.noreply.github.com>
2023-01-06 17:08:51 +0530

    Add files via upload

--- react fiile/App.js
+++ react fiile/App.js
@@ -1,9 +1,10 @@
 //import logo from './logo.svg';
 //import './App.css';
+import React from 'react';
 
-import ExpenseItem from './component/ExpenseItem'; //using capital letter so that we can identify it as custom component
+import Expenses from './component/Expenses'; //using capital letter so that we can identify it as custom component
 
-function App() {
+const App= ()=> {
   const expenses = [
     {
       id: 'e1',
@@ -26,46 +27,9 @@ function App() {
     },
   ];
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-         
-    //   </header>
-    //   <ExpenseItem></ExpenseItem> 
-    // </div>
     <div>
       <h2>ExpenseItem</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      ></ExpenseItem>
+      <Expenses items={expenses}/>
     </div>
   );
 }
